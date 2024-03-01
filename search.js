277d13b991a79479461a1ebb879c8ea84390b59e
Luis Garrillo <lggarrillo@urbe.edu.ve>
2024-02-29 23:12:03 -0400

    binary search vs linear search

--- search.js
+++ search.js
@@ -1,3 +1,5 @@
+const sort = require("./sorting"); 
+
 function binarySearch(array, value) {
     if (!Array.isArray(array)) {
         throw new TypeError("binarySearch only accepts arrays!");
@@ -7,27 +9,51 @@ function binarySearch(array, value) {
     searchArray = array;
     if (searchArray.length <= 1) {
         if (searchArray.length == 1 && searchArray[0] == value) {
-            console.log("Value found at position: 1");
+            console.log(value + " found in the list");
             return true;
         }
         else {
-            console.log("Value not found");
+            console.log(value + " not found in the list");
             return false;
         }
     }
     let halfIndex = Math.ceil(searchArray.length/2);
     
     if (searchArray[halfIndex] < value) {
-        binarySearch(searchArray.slice(halfIndex));
+        binarySearch(searchArray.slice(halfIndex), value);
     }
     else if (searchArray[halfIndex] > value) {
-        binarySearch(searchArray.slice(0, halfIndex));
+        binarySearch(searchArray.slice(0, halfIndex), value);
     }
     else {
-        console.log("Value found at position: " + (halfIndex + 1));
+        console.log(value + " found in the list");
         return true;
     }
 }
 
-let testArray = [1, 2, 3, 4, 5];
-binarySearch(testArray, 2);
+function linearSearch(array, value) {
+    if (!Array.isArray(array)) {
+        throw new TypeError("binarySearch only accepts arrays!");
+    }
+
+    for (let i = 0, length = array.length; i < length; i++) {
+        if (array[1] == value) {
+            console.log(value + " found in the list");
+            return true;
+        }
+    }
+
+    console.log(value + " not found in the list");
+    return true;
+}
+
+let testArray = Array.from({length: 10000000}, () => Math.floor(Math.random() * 10000000));;
+testArray = sort.mergeSort(testArray);
+
+console.time("Binary search runtime");
+binarySearch(testArray, 4);
+console.timeEnd("Binary search runtime")
+
+console.time("Linear search runtime");
+linearSearch(testArray, 4);
+console.timeEnd("Linear search runtime")
