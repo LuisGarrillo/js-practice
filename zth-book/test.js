b6e95d0abcf8b270f2e8413ef2621026c7666a35
Luis Garrillo <lggarrillo@urbe.edu.ve>
2024-03-05 11:23:43 -0400

    for of and for in

--- zth-book/test.js
+++ zth-book/test.js
@@ -22,15 +22,23 @@ class Person {
 // inheritance
 class Student extends Person {
     constructor(name, age, career) {
-        super(name, age)
-        this.career = career
+        super(name, age);
+        this.career = career;
     }
 }
 
-let studentInfo = {}
-
-let studentA = new Student("Luis", 19, "Informatic Engineering")
+let studentInfo = {};
+let testArray = [1,2,3,4,5,6];
+let studentA = new Student("Luis", 19, "Informatic Engineering");
 
 for (const key in studentA) {
     console.log(capitalizeArrow(key) + ": " + studentA[key]);
+}
+
+for (const i in testArray) {
+    console.log(i);
+}
+
+for (const element of testArray) {
+    console.log(element);
 }
